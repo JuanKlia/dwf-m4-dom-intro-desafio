78fd0117495666669e990122f926858aad526af2
unknown <juankliabaras@gmail.com>
2022-03-04 18:24:30 -0300

    modificaciones en .JS realizadas

--- index.js
+++ index.js
@@ -25,6 +25,41 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function agregarEtiquetasAlDom(arrayLi) {
+  const el = document.querySelector(".lista");
+  arrayLi.forEach((x) => {
+    el.appendChild(x);
+  });
+}
+
+function crearLi(arrayObjetos) {
+  const arrayLi = arrayObjetos.map((cadaObjeto) => {
+    const el = document.createElement("li");
+    el.textContent = JSON.stringify(cadaObjeto);
+
+    if (cadaObjeto.class == "special") {
+      el.classList.add("special");
+    }
+    return el;
+  });
+
+  return arrayLi;
+}
+
+function eliminarLi() {
+  const liEl = document.querySelectorAll(".lista li");
+  liEl.forEach((x) => {
+    x.remove();
+  });
+
+  return liEl;
+}
+
+function main() {
+  eliminarLi();
+  console.log(" tags <li> eliminados");
+  const arrayLi = crearLi(cosasQueAprendimos);
+  agregarEtiquetasAlDom(arrayLi);
+}
 
 main();
